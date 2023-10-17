a6a7c79f759f82ee15bd0a7a2c2045f44345a076
VillSource <anirut1800@gmail.com>
2023-10-17 23:30:55 +0700

    feat: 📃load post fillter by profile

--- src/app/features/profile/pages/profile-page/profile-page.component.ts
+++ src/app/features/profile/pages/profile-page/profile-page.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, switchMap } from 'rxjs';
+import { Observable, of, switchMap, tap } from 'rxjs';
 import { AuthorityService } from 'src/app/core/auth/authority.service';
+import { PostPreviewAndAuthor } from 'src/app/core/models/post-and-author';
 import { User } from 'src/app/core/models/user';
+import { LazyPostService } from 'src/app/core/services/lazy-post.service';
 import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
+  providers: [LazyPostService]
 })
 export class ProfilePageComponent implements OnInit {
   author: User | undefined;
   isMyId = false;
+
+  loadPostsProxy: ()=>void = ()=>{};
+  posts: PostPreviewAndAuthor[] = [];
+
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
-    private auth: AuthorityService
+    private auth: AuthorityService,
+    private lazyPostService: LazyPostService
   ) {}
 
   ngOnInit(): void {
@@ -31,10 +39,36 @@ export class ProfilePageComponent implements OnInit {
       .subscribe((author) => {
         this.author = author;
         this.isMyId = this.auth.user_id === this.author?.id;
+        this.loadPostsProxy = ()=> this.loadPosts(author?.id);
+        this.listenPostChange();
       });
   }
 
   getAvatar() {
     return '/api/img/v1/avatar/' + this.author?.id;
+  }
+
+  private loadPosts(author?:string){
+    this.lazyPostService.loadMore(author)
+  }
+
+  listenPostChange(){
+    this.lazyPostService.posts$
+      .asObservable()
+      .pipe(tap(console.log)).subscribe({
+        next: p=>{this.posts = p}
+      });
+    this.loadPostsProxy();
+  }
+
+  postTrackBy(index: number, item: PostPreviewAndAuthor) {
+      return item.postPreview.id;
+  }
+
+  removeFromId(id: string) {
+    this.posts.splice(
+      this.posts.findIndex((item) => item.postPreview.id === id),
+      1
+    );
   }
 }
