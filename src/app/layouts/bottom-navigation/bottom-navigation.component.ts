2d69dd671cd634e24c06a09ee7f444d4e160638c
athenexfolk <folkadpocket99@gmail.com>
2023-12-09 01:59:52 +0700

    change bottom navigation and redirect layout

--- src/app/layouts/bottom-navigation/bottom-navigation.component.ts
+++ src/app/layouts/bottom-navigation/bottom-navigation.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-navigation',
   standalone: true,
-  imports: [CommonModule, NavigationComponent],
+  imports: [
+    CommonModule,
+    //NavigationComponent,
+    RouterLink,
+  ],
   templateUrl: './bottom-navigation.component.html',
-  styleUrl: './bottom-navigation.component.scss'
+  styleUrl: './bottom-navigation.component.scss',
 })
 export class BottomNavigationComponent {
+  @ViewChild('floatButton') floatButton!: ElementRef<HTMLButtonElement>;
+  lastScrollPosition = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollEvent() {
+    const currentScrollPosition = window.scrollY;
+    if (currentScrollPosition > this.lastScrollPosition) {
+      this.floatButton.nativeElement.style.bottom = '-72px';
+    } else if (currentScrollPosition < this.lastScrollPosition) {
+      this.floatButton.nativeElement.style.bottom = '16px';
+    }
+    this.lastScrollPosition = currentScrollPosition;
+  }
 }
